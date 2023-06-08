3698cf8335ce14efedb6ba6ac7291cf1b901b435
bradleycao <hungcao1995@gmail.com>
2023-06-08 12:38:58 -0400

    Created Bar Charts

--- mainscript.py
+++ mainscript.py
@@ -67,3 +67,20 @@ with right_column:
 
 st.markdown("---")
 
+# Sales by Product Line (Bar Chart)
+
+sales_by_product_line = (
+    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+)
+
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x = "Total",
+    y = sales_by_product_line.index,
+    orientation = "h",
+    title = "<b>Sales by Product Line </b>",
+    color_discrete_sequence = ["#0083B8"] * len(sales_by_product_line),
+    template = "plotly_white"
+)
+
+st.plotly_chart(fig_product_sales)
